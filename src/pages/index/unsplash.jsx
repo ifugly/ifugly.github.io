0fba75bf2658537966333e039330c9c50bd75829
ifugly <360944719@qq.com>
2023-07-03 12:44:31 +0800

    添加搜素组件

--- src/pages/index/unsplash.jsx
+++ src/pages/index/unsplash.jsx
@@ -5,13 +5,14 @@ export default function GetUrl() {
 
     useEffect(() => {
         let urlCancelled = false // 防止重放
-        fetch("https://api.unsplash.com/photos/random/?collections=317099&orientation=landscape&client_id=gtJVuXhWYnhx1JZjQnjRiGvvd0pWMdKEtcGSjvoJ3UQ")
-            .then((response) => response.json())
-            .then((data) => {
-                if (!urlCancelled) {
-                    setUrl(data.urls.regular);
-                }
-            }); // avoid setState loop
+        // fetch("https://api.unsplash.com/photos/random/?collections=317099&orientation=landscape&client_id=gtJVuXhWYnhx1JZjQnjRiGvvd0pWMdKEtcGSjvoJ3UQ")
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        if (!urlCancelled) {
+            // setUrl(data.urls.regular);
+            setUrl('https://images.unsplash.com/photo-1582148432011-85c462550e5f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0Njg5Njl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODgzNTg5NTZ8&ixlib=rb-4.0.3&q=80&w=1080');
+        }
+        // }); // avoid setState loop
         return () => {
             urlCancelled = true;
         }
